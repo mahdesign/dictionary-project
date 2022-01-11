62497436f24e24863f2594bd936f165093117356
Mahsa Irankhah <mahsairankhah77@gmail.com>
2022-01-11 22:01:44 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="img-fluid App-logo" />
+      </header>
+    </div>
+  );
+}
